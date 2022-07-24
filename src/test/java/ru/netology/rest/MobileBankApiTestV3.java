ce1ce8ad0a576c5294fde71d9259cd6c8c497402
trukhachev495 <sergey.trainings@gmail.com>
2022-07-24 16:59:55 +0300

    тест прошел

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -22,7 +22,7 @@ public class MobileBankApiTestV3 {
                 // специализированные проверки - лучше
                 .contentType(ContentType.JSON)
                 .body("", hasSize(3))
-                .body("[0].currency", equalTo("RUB"))
+                .body("[2].currency", equalTo("RUB"))
                 .body("[0].balance", greaterThanOrEqualTo(0))
         ;
     }
